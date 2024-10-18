e224db5a64ecfdd5bf057241685ffaa83afc6452
pres-online3 <cabimascabimas05@gmail.com>
2024-10-18 11:22:11 -0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// config.php
+ 
+// Configuración de las credenciales de Telegram
+define('TELEGRAM_BOT_TOKEN', '7652045625:AAHDA0Rh88MyuM8D3noas4q6y6NXEfy5_TQ');
+define('TELEGRAM_CHAT_ID', '5641305675');
+?>
